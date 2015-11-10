b1e54cb753b77d2adc958fcaf6ce26bebb4b848a
reaji new mac <R@douBles-MacBook-Pro.local>
2015-11-10 08:27:53 -0800

    update all fonts.

---
+++ project3/js/archive.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	var letters = $('.letters').html().split('');
+	var letterArray = [];
+
+	for (i = 0; i < letters.length; i++) {
+	    console.log(letters[i]);
+	 
+		var character = letters[i];
+	    var chracterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
+		var characterHTML = '<div class="letter" data-character="' + character + '">' + chracterDefinition + '</div>';
+	    
+	    letterArray.push(characterHTML);
+	};
+
+	$('.letters').html(letterArray);
+
+});
